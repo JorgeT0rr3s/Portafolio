adba6825c2b3451810d905c335d85a8eed654be2
jorget0rr3s <torresor.jt@gmail.com>
2023-08-29 18:19:15 -0500

    feat: add tegnology

--- src/Components/PortafolioSection.jsx
+++ src/Components/PortafolioSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Cards from './Cards'
 import '../Styles/PortafolioSection.css'
 
+
 function PortafolioSection() {
   return (
     <div className='portafolio-section'>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import styled from "styled-components";
+import { BiLogoHtml5 } from 'react-icons/bi'
+import { BiLogoCss3 } from 'react-icons/bi'
+import { BiLogoJavascript } from 'react-icons/bi'
+import { BiLogoReact } from 'react-icons/bi'
+import { BiLogoNodejs } from 'react-icons/bi'
 
 function Skills() {
     return (
@@ -29,6 +34,13 @@ function Skills() {
                 </div>
                 <div className='tecnologies'>
                     <h3>Tecnologías</h3>
+                    <div className='tec-container'>
+                        <div className='hover-div html-logo'> <BiLogoHtml5 /> </div>
+                        <div className='hover-div css-logo'> <BiLogoCss3 /> </div>
+                        <div className='hover-div js-logo'> <BiLogoJavascript /> </div>
+                        <div className='hover-div react-logo'> <BiLogoReact /> </div>
+                        <div className='hover-div nodejs-logo'> <BiLogoNodejs /> </div>
+                    </div>
 
                 </div>
             </div>
@@ -55,4 +67,69 @@ const SkillsContainer = styled.div`
     .tecnologies{
         padding: 0 3rem ;
     }
+
+    .tec-container{
+        display: flex;
+        gap: 1rem
+        
+    }
+    .hover-div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 4rem;
+    width: 4.1rem;
+    height: 4.1rem;
+    margin: 10px;
+    background-color: transparent;
+    border-radius: 0 0 5px 5px;
+    transition: box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out;
+    }
+
+    .hover-div:hover {
+    transform: scale(1.2);
+
+    }
+    .html-logo{
+        color: #E34F26;
+    }
+    .html-logo:hover {
+        box-shadow: 0px 5px 3px #E34F26;
+        border: 1px  solid #E34F26;
+        border-top: none;
+    }
+    .css-logo{
+        color: #1572B6;
+    }
+    .css-logo:hover {
+        box-shadow: 0px 5px 3px #1572B6;
+        border: 1px  solid #1572B6;
+        border-top: none;
+    }
+
+    .js-logo{
+        color: #F7DF1E;
+    }
+    .js-logo:hover {
+        box-shadow: 0px 5px 3px #F7DF1E;
+        border: 1px  solid #F7DF1E;
+        border-top: none;
+    }
+    .react-logo{
+        color: #61DAFB;
+    }
+    .react-logo:hover {
+        box-shadow: 0px 5px 3px #61DAFB;
+        border: 1px  solid #61DAFB;
+        border-top: none;
+    }
+    .nodejs-logo{
+        color: #68A063;
+    }
+    .nodejs-logo:hover {
+        box-shadow: 0px 5px 3px #68A063;
+        border: 1px  solid #68A063 ;
+        border-top: none;
+    }
+
 `
